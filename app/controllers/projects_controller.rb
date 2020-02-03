c3a28e8581d6f0da2cf752793e3b75fd5ff37ec6
115cm <59826145+115cm@users.noreply.github.com>
2020-02-03 21:55:58 +0900

    アプリ立ち上げ

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,23 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    Project.create(project_params)
+    redirect_to root_path
+  end
+
+
+  private
+
+  def project_params
+    params.require(:project).permit(:text)
+  end
+
+end
